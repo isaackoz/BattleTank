c025199d2c9beaf047f22c3836b083f4c5b11132
DESKTOP-4CI3BRI\Isaac <isaackoz130@gmail.com>
2018-07-03 23:28:57 -0700

    created projectile fire input and class

--- BattleTankGame/Source/BattleTankGame/TankAimingComponent.cpp
+++ BattleTankGame/Source/BattleTankGame/TankAimingComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TankTurret.h"
 #include "TankAimingComponent.h"
+#include "TankTurret.h"
+
 
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
